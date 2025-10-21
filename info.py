825dd75222fb950023177d5959bb3d2863cec130
PrajwalPatil-01 <prajwalpatilxp@gmail.com>
2025-10-21 10:35:52 +0530

    Update shortlink configuration defaults

--- info.py
+++ info.py
@@ -31,6 +31,6 @@ DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://FileToLink:FileToLink@
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
+SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'nanolinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'fe3a994196cb2bc224087f50c67e3419112b349f')
